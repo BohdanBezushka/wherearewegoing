b5af521febbf5b6635a54382fb73da706e6e4f03
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-18 18:37:07 +0000

    Add basic structure for the homepage

--- wherearewegoing/settings.py
+++ wherearewegoing/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-#Allauth
+# Allauth
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
