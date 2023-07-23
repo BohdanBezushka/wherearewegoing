d344f0a7440f045ed7820186226bd03c86e0d72b
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-23 19:43:49 +0000

    Add the FestivalAdmin class in admin.py file

--- festivals/admin.py
+++ festivals/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Festival
 
 # Register your models here.
+
+class FestivalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'location', 'price')
+    list_filter = ('date', 'location')
+    search_fields = ('name', 'location')
+
+admin.site.register(Festival, FestivalAdmin)
