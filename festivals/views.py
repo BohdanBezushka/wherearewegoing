440cc1f065f47893d7cea7da99252b0eaf226988
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-27 19:06:06 +0000

    Add the view all_festival to show all festivals

--- festivals/views.py
+++ festivals/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Festical
 
 # Create your views here.
+
+
+def all_festivals(request):
+    """ A view to show all festivals, including sorting and search queries """
+
+    return render(request, 'festivals/festivals.html', context)
