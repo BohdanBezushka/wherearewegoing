70a40ed6d8aadab3a53a413fe8106548061a9f41
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-29 13:13:12 +0000

    Add a new URL path to urls.py

--- festivals/urls.py
+++ festivals/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_festivals, name='festivals')
+    path('', views.all_festivals, name='festivals'),
+    path('<festival_id>', views.festival_detail, name='festival_detail'),
 ]
