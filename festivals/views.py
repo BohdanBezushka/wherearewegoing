1d0c14017d6b36d0ad9cdbfbb00c39637bc9e5ed
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-29 13:05:10 +0000

    Add festival_detail view for individual festival pages

--- festivals/views.py
+++ festivals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Festival
 
 # Create your views here.
@@ -8,10 +8,20 @@ def all_festivals(request):
     """ A view to show all festivals and search queries """
 
     festivals = Festival.objects.all()
-    print(festivals)
 
     context = {
         'festivals': festivals,
     }
 
     return render(request, 'festivals/festivals.html', context)
+
+def festival_detail(request, festival_id):
+    """ A view to show individual festival details"""
+
+    festival = get_object_or_404(Festival, pk=festival_id)
+
+    context = {
+        'festival': festival,
+    }
+
+    return render(request, 'festivals/festival_detail.html', context)
