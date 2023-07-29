448bebb3d99c5dd8f4e1fdda4d70b0598540386c
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-07-29 14:14:08 +0000

    Add search funcionality

--- festivals/views.py
+++ festivals/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Festival
 
 # Create your views here.
@@ -8,9 +10,22 @@ def all_festivals(request):
     """ A view to show all festivals and search queries """
 
     festivals = Festival.objects.all()
+    query = None
+
+    """User can search in search bar"""
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('festivals'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            festivals = festivals.filter(queries)
 
     context = {
         'festivals': festivals,
+        'search_term': query,
     }
 
     return render(request, 'festivals/festivals.html', context)
