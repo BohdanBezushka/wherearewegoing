68262919680b8fcb6731fafc8ed711afa54f68c3
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-02 11:18:30 +0000

    Add the form for the user

---
+++ checkout/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import UserData
+
+
+class UserDataForm(forms.ModelForm):
+    class Meta:
+        model = UserData
+        fields = ('full_name', 'email', 'phone_number',
+                  'user_id', 'date_of_birth',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field.
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'user_id': 'ID',
+            'date_of_birth': 'Date of Birth',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
