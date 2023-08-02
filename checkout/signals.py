9da1bfd5a98178de40719e022772fc10f1c82add
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-02 11:09:18 +0000

    Add signals file to create, save and delete

---
+++ checkout/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import Ticketing
+
+
+@receiver(post_save, sender=Ticketing)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on line ticket update/create.
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=Ticketing)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on line ticket delete.
+    """
+    instance.order.update_total()
