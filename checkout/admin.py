470501437f7a3350e4b558c93418df33700a223f
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-02 11:00:05 +0000

    Add the UserData and Ticketing models to the admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import UserData, Ticketing
 
 # Register your models here.
+class TicketingAdminInline(admin.TabularInline):
+    model = Ticketing
+    readonly_fields = ('lineitem_total',)
+
+
+class UserDataAdmin(admin.ModelAdmin):
+    inlines = (TicketingAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'user_id',
+              'date_of_birth', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(UserData, UserDataAdmin)
