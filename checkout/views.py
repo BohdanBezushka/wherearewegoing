ed647569bd272c7c5a23706a3d3c64c72a9cf3f3
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-02 13:20:15 +0000

    Add the checkout view to fill in your purchase details

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import UserDataForm
+
+# This is the web page when the user wants to buy ticktes.
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment.")
+        return redirect(reverse('festivals'))
+
+    userdata_form = UserDataForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'userdata_form': userdata_form,
+    }
+
+    return render(request, template, context)
