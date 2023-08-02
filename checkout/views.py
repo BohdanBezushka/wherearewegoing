7b16b1b20174b95c259b1f06070630a415db7988
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-02 21:10:59 +0000

    Add a success page after making the payment

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import UserDataForm
+from .models import UserData, Ticketing
+from festivals.models import Festival
 from bag.contexts import bag_contents
 
 import stripe
@@ -14,24 +16,66 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment.")
-        return redirect(reverse('festivals'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'user_id': request.POST['user_id'],
+            'date_of_birth': request.POST['date_of_birth'],
+        }
+        order_form = UserDataForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    festival = Festival.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        ticketing = Ticketing(
+                            order=order,
+                            festival=festival,
+                            quantity=item_data,
+                        )
+                        ticketing.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            ticketing = Ticketing(
+                                order=order,
+                                festival=festival,
+                                quantity=quantity,
+                                festival_size=size,
+                            )
+                            ticketing.save()
+                except Festival.DoesNotExist:
+                    messages.error(request, (
+                        "One of the festivals in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment.")
+            return redirect(reverse('festivals'))
 
-    print(stripe_public_key)
-    print(stripe_secret_key)
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-    userdata_form = UserDataForm()
+        userdata_form = UserDataForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -42,6 +86,27 @@ def checkout(request):
         'userdata_form': userdata_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(UserData, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
