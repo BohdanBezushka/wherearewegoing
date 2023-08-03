6072c5a1c26cbcc8520fddf026839eeb9de9f5af
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-03 12:36:57 +0000

    Add UserProfile model for order history

--- checkout/models.py
+++ checkout/models.py
@@ -5,10 +5,13 @@ from django.db.models import Sum
 from django.conf import settings
 
 from festivals.models import Festival
+from profiles.models import UserProfile
 
 # Information I need from the user to send the tickets.
 class UserData(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
