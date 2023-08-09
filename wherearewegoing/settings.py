d8abaaeb99c14cb38d9ade9c5d810eb61d7d68de
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-08-09 21:28:24 +0000

    Configure the settings file to use gmail

--- wherearewegoing/settings.py
+++ wherearewegoing/settings.py
@@ -243,12 +243,12 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # Email configuration:
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'wherearewegoing.example.com'
+    DEFAULT_FROM_EMAIL = 'wherearewegoing@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'wherearewegoing.festivals@gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
